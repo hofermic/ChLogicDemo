4257515ebfb45ec3345b04fc8a9aa16b719a3ba0
LAPTOP-LA3MF1O2\mikeh <mike.hofer@outlook.com>
2023-05-17 16:52:50 -0400

    Add project files.

---
+++ ChLogicDemo/Controllers/ControllerBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChLogicDempo.Controllers
+{
+    public class ControllerBase : Controller
+    {
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            ViewBag.ErrorMessage = "An error occurred: " + filterContext.Exception.Message;
+            filterContext.ExceptionHandled = true;
+            filterContext.Result = new ViewResult
+            {
+                ViewName = "~/Views/Shared/Error.cshtml"
+            };
+
+        }
+    }
+}
---
+++ ChLogicDemo/Controllers/HierarchyController.cs
@@ -0,0 +1,20 @@
+﻿using ChLogicDempo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChLogicDempo.Controllers
+{
+    public class HierarchyController : ControllerBase
+    {
+        private DemoDbEntities db = new DemoDbEntities();
+        // GET: Hierarchy
+        public ActionResult Index()
+        {
+            var managers = db.Managers.ToList();
+            return View(managers);
+        }
+    }
+}
